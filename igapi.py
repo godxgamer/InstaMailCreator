a7c9c970a74a3bc91905edd2703bc7192cc9ec15
godxgamer <134595794+godxgamer@users.noreply.github.com>
2023-09-07 17:11:52 +0530

    Update igapi.py

--- igapi.py
+++ igapi.py
@@ -1,34 +1,43 @@
 import requests
 
-
 headers={
     "Content-Type": "application/json",
 }
-proc=f"enter your proxy here" # "username:password@host:port"  (without http:// or https:// )
-num="enter your number here" # example "919988776655" (without + )
-data={
-    "api-key":"gamer",
-    "number": num,
-    "proxy":proc
-}
 
-r=requests.post("http://128.140.99.16:6969/api/send-sms",json=data,headers=headers,timeout=100)
-if "securetoken" in r.text:
-    print(r.text)
-    r=r.json()
-    securetoken=r['message']['securetoken']
-    otp=str(input("otp : "))
+proc = str(input("Enter Your Proxy ->"))  # "username:password@host:port"  (without http:// or https:// )
+num = str(input("Enter Your Number ->"))  #919988998877
+
+def creator():
     data={
-    "api-key":"gamer",
-    "securetoken": securetoken,
-    "proxy":proc,
-    "otp":otp
+        "api-key":"gamer",
+        "number": num,
+        "proxy":proc
     }
 
-    r=requests.post("http://128.140.99.16:6969/api/create-acc",json=data,headers=headers,timeout=100)
-    print(r.text)
+    r=requests.post("http://128.140.99.16:6969/api/send-sms",json=data,headers=headers,timeout=100)
+    if "securetoken" in r.text:
+        print(r.text)
+        r=r.json()
+        securetoken=r['message']['securetoken']
+        otp=str(input("otp : "))
+        data={
+        "api-key":"gamer",
+        "securetoken": securetoken,
+        "proxy":proc,
+        "otp":otp
+        }
 
+        r=requests.post("http://128.140.99.16:6969/api/create-acc",json=data,headers=headers,timeout=100)
+        print(r.text)
 
+    elif "IndexError" in r.text:
+        print("indexerror restarting")
+        creator()
 
-else:
-    print(r.text)
+    elif "ReadTimeout" in r.text:
+        print("ReadTimeout restarting")
+        creator()
+    else:
+        print(r.text)
+
+creator()
