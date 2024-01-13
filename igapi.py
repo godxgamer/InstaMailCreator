bb1665d1e4ad3898a57dc32ec96dc445526e8a56
godxgamer <134595794+godxgamer@users.noreply.github.com>
2024-01-13 13:59:37 +0530

    Update igapi.py

--- igapi.py
+++ igapi.py
@@ -1,52 +1,66 @@
 import requests
 
-headers={
+# API headers
+headers = {
     "Content-Type": "application/json",
-    "X-API-Key":"None",
+    "X-API-Key": "None",  # Replace with your actual API key
 }
 
-baseurl='http://128.140.99.16:5622' # Base Url Of Api
+# Base URL of the API
+base_url = 'http://128.140.99.16:5622'
+
+# Email for Instagram registration
+email = 'cekkukofyi@gufum.com'  # Note Always Use least Flagged Domains for email like gmail,outlook,hotmailInstagram Is Very Strict With Email Account Registratin They Block or susspend Account Registered With Temp Mails
 
 
-emal='cekkukofyi@gufum.com'  # Note Always Use least Flagged Domains for email like gmail,outlook,hotmail
-#Instagram Is Very Strict With Email Account Registratin They Block or susspend Account Registered With Temp Mails
+# Proxy details for making requests
+proxy_path = 'username:password@host:port'  # Replace with your actual proxy details. username of your paid proxy " : " password of Your proxy " @ " Host of proxy " : " Port of proxy  " example =   smartuser:ERvccvRRTgfd@gate.smartptoxy.com:7777 You Can Also Use Free Proxy Of Any Country " Only Proxy Should Be alive
 
-proxy_path='username:password@host:port' # username of your paid proxy " : " password of Your proxy " @ " Host of proxy " : " Port of proxy  " example =   smartuser:ERvccvRRTgfd@gate.smartptoxy.com:7777
-# You Ca Also Use Free Proxy Of Any Country " Only Proxy Should Be alive
+# Country code for advanced features Of This Api This Will Make Country Info Spoofing To Reduce Bot Detection By Instagram
+country_code = 'IN'
 
-country_Code='IN' # "India"   This Is A Advance Feature Of This Api This Will Make Country Info Spoofing To Reduce Bot Detection By Instagram
-" Example If You Are Using Indian Proxy Location Then Country Code should be IN"
-
-
-data={
-    "email":emal,
-    "proxy":proxy_path,
-    "country_code":country_Code
+# Data payload for the API request
+data = {
+    "email": email,
+    "proxy": proxy_path,
+    "country_code": country_code
 }
 
-sendsms=requests.post(url=f'{baseurl}/api/insta/android/email/send',json=data,headers=headers,timeout=200)  # This Request Will Send An Sms containig otp To Yor email
-if 'user_agent' in sendsms.text: # an if else condition To check the given keyword is in response or not
-    print('otp sent ')  # Otp sent sucessfull To your email.......
-    otp=str(input("enter otp "))
-    data={
-        "otp":otp,
-        "proxy":proxy_path,
-        "client_data":sendsms.json(), #  This will take response from first api ("sendsms")  and pass into the next api  .
+# Sending an SMS containing OTP to the provided email
+sendsms = requests.post(
+    url=f'{base_url}/api/insta/android/email/send',
+    json=data,
+    headers=headers,
+    timeout=200
+)
+
+# Checking if 'user_agent' is in the response text
+if 'user_agent' in sendsms.text:
+    print('OTP sent successfully to your email...')
+    otp = str(input("Enter OTP: "))
+
+    # Data for the second API request, including the response from the first API
+    data = {
+        "otp": otp,
+        "proxy": proxy_path,
+        "client_data": sendsms.json()
     }
-    create=requests.post(url=f'{baseurl}/api/insta/android/email/create',json=data,headers=headers,timeout=200).json()  # At Last Create Api .This Will Take otp,proxy and sendsms data to verify and create account using email
+
+    # Verifying OTP and creating an account using email
+    create = requests.post(
+        url=f'{base_url}/api/insta/android/email/create',
+        json=data,
+        headers=headers,
+        timeout=200
+    ).json()
+
     print(create)
-
 else:
-    print(sendsms.text)  # in case otp not sent sucessfull
-
-
+    print(sendsms.text)
 
 """
+You can use this API in any language (py, php, js, Go, java) to create your own account registration automation.
 
-You Can Use This Api According To You In Any Language (py,php,js,Go,java) and Make Your Own Automation Of account registragtion
-
-
-Contact For Bulk Account Creation 
-Join @gxbytes For updates and new upcoming freatures like ( Post Upload api, Get followers and following list api ,follow api , like api, ............. and all features of instagram of api ) 
-If You Have Any query About This Api You Can Contact me on telegram . my username = '@god_x_gamer'
+For bulk account creation or any queries, join @gxbytes for updates and upcoming features. 
+If you have any queries about this API, you can contact me on Telegram. My username is '@god_x_gamer'.
 """
