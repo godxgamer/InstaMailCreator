771478a9b8af99011f6392164202df28dd3f864c
Gamer <134595794+godxgamer@users.noreply.github.com>
2024-01-20 13:16:08 +0530

    Update igapi.py

--- igapi.py
+++ igapi.py
@@ -7,17 +7,17 @@ headers = {
 }
 
 # Base URL of the API
-base_url = 'http://128.140.99.16:5622'
+base_url = 'http://127.0.0.1:5000'
 
 # Email for Instagram registration
 email = 'cekkukofyi@gufum.com'  # Note Always Use least Flagged Domains for email like gmail,outlook,hotmailInstagram Is Very Strict With Email Account Registratin They Block or susspend Account Registered With Temp Mails
 
 
 # Proxy details for making requests
-proxy_path = 'username:password@host:port'  # Replace with your actual proxy details. username of your paid proxy " : " password of Your proxy " @ " Host of proxy " : " Port of proxy  " example =   smartuser:ERvccvRRTgfd@gate.smartptoxy.com:7777 You Can Also Use Free Proxy Of Any Country " Only Proxy Should Be alive
+proxy_path = f'proxy-username:proxy-password@proxy-host:proxy-port'  # Replace with your actual proxy details. username of your paid proxy " : " password of Your proxy " @ " Host of proxy " : " Port of proxy  " example =   smartuser:ERvccvRRTgfd@gate.smartptoxy.com:7777 You Can Also Use Free Proxy Of Any Country " Only Proxy Should Be alive
 
 # Country code for advanced features Of This Api This Will Make Country Info Spoofing To Reduce Bot Detection By Instagram
-country_code = 'IN'
+country_code = 'US'
 
 # Data payload for the API request
 data = {
@@ -25,6 +25,17 @@ data = {
     "proxy": proxy_path,
     "country_code": country_code
 }
+
+#Guys If Want to pass Your Username And Password
+# add two parameters data =
+"""{
+    "full_name":'gamer', 
+    "password":'gamerrrrr',
+    "email": email,
+    "proxy": proxy_path,
+    "country_code": country_code
+}
+"""
 
 # Sending an SMS containing OTP to the provided email
 sendsms = requests.post(
